ee924bb4d87d3688ba23403fd9c72d5ea7b9b652
Peter Deyi <peterdeyi@icloud.com>
2023-05-07 12:03:21 -0700

    Initial Commit

---
+++ mediTransApp/mediTransAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  mediTransAppApp.swift
+//  mediTransApp
+//
+//  Created by Peter Deyi on 5/7/23.
+//
+
+import SwiftUI
+
+@main
+struct mediTransAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
